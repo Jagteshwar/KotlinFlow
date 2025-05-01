475b54200dab50b2625141a9602e976936474f1a
Jagteshwar <jagteshwars3@gmail.com>
2025-05-01 14:03:13 -0400

    shared flow

--- app/src/main/java/com/jagteshwar/kotlinflow/MainActivity.kt
+++ app/src/main/java/com/jagteshwar/kotlinflow/MainActivity.kt
@@ -8,8 +8,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
@@ -19,25 +18,34 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         GlobalScope.launch(Dispatchers.Main) {
-            producer()
-                .flowOn(Dispatchers.IO)
-                .collect {
-                  //  Log.d(TAG, it.toString())
-                    Log.d(TAG, "Collector thread ${Thread.currentThread().name}")
+            val result = producer()
+                result.collect {
+                    Log.d(TAG, "First collector-> $it")
+                   // Log.d(TAG, "Collector thread ${Thread.currentThread().name}")
 
                 }
         }
 
-    }
-
-    private fun producer(): Flow<Int> {
-        return flow {
-            val list = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-                list.forEach {
-                    delay(1000)
-                    Log.d(TAG, "Emitter thread ${Thread.currentThread().name}")
-                    emit(it)
+        GlobalScope.launch(Dispatchers.Main) {
+            val result = producer()
+            delay(2500)
+            result.collect {
+                Log.d(TAG, "Second collector-> $it")
             }
         }
+
+    }
+
+    @OptIn(DelicateCoroutinesApi::class)
+    private fun producer(): Flow<Int> {
+      val mutableSharedFlow = MutableSharedFlow<Int>(2)
+        GlobalScope.launch {
+            val list = listOf(1, 2, 3, 4, 5)
+            list.forEach {
+                mutableSharedFlow.emit(it)
+                delay(1000)
+            }
+        }
+        return mutableSharedFlow
     }
 }
